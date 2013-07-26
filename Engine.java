823187b71c2559a1d696da9e326ca8445104e6ba
Zigsfi <arthuberman94@gmail.com>
2013-07-25 20:45:51 -0400

    more files

--- Engine.java
+++ Engine.java
@@ -6,11 +6,17 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-public class Engine extends Canvas {
+public class Engine extends Canvas implements MouseListener, MouseMotionListener{
 
 
 	/**
@@ -22,6 +28,7 @@ public class Engine extends Canvas {
 	private BufferedImage buffImg;
 	private int width, height;
 	private int resX, resY;
+	public int mousex, mousey;
 	private int camX, camY, camSX, camSY;
 	final EngineHost eng;
 	public Engine(EngineHost e, int w, int h, int rx, int ry, int layers) {
@@ -58,7 +65,7 @@ public class Engine extends Canvas {
 	}
 
 	public void paint(Graphics g) {
-		Graphics off = buffImg.getGraphics();
+		Graphics2D off = (Graphics2D) buffImg.getGraphics();
 		off.setColor(Color.white);
 		off.fillRect(0, 0, resX, resY);
 		for (int i = 0; i < layer.length; i++) {
@@ -73,12 +80,68 @@ public class Engine extends Canvas {
 			d = drawIt.next();
 			d.draw(layer);
 		}
+
 		for (int i = 0; i < layer.length; i++) {
 			off.drawImage(layer[i], 0, 0, resX, resY, camX, camY, camX + camSX, camY + camSY, this);
 		}
+		renderLights(off);
+
 		g.drawImage(buffImg, 0, 0, resX, resY, this);
 	}
+	public void renderLights(Graphics2D g) {
+		Light l = new Light(mousex, mousey, 500);
+		
+		ShadowCaster s = new ShadowCaster(70, 70, 40, 40);
+		renderShadow(l, s, g);
+		s = new ShadowCaster(170, 70, 40, 40);
+		renderShadow(l, s, g);
+
+		l = new Light(300, 300, 500);
+		renderShadow(l, s, g);
+		g.setPaint(l.gradient);
+		g.fill(new Rectangle(0, 0, 1000, 1000));
+	}
 	
+	public void renderShadow(Light l, ShadowCaster s, Graphics g) {
+		g.setColor(Color.black);
+		g.drawOval(l.xPos, l.yPos, 10, 10);
+		g.fillRect(s.xPos, s.yPos, s.width, s.height);
+		//g.drawLine(50, 50, 50 + (s.xPos - 50) * l.range, 50 + (s.yPos + s.height - 50) * l.range);
+		Polygon shadow = new Polygon();
+		shadow.addPoint(s.xPos,  s.yPos);
+		shadow.addPoint(s.xPos + l.range * (s.xPos - l.xPos), s.yPos + l.range * (s.yPos - l.yPos));
+		shadow.addPoint(s.xPos + s.width + l.range * ((s.xPos + s.width) - l.xPos), s.yPos + l.range * (s.yPos - l.yPos));
+		shadow.addPoint(s.xPos + s.width, s.yPos);
+		g.fillPolygon(shadow);
+		
+		shadow = new Polygon();
+		shadow.addPoint(s.xPos,  s.yPos);
+		shadow.addPoint(s.xPos + l.range * (s.xPos - l.xPos), s.yPos + l.range * (s.yPos - l.yPos));
+		shadow.addPoint(s.xPos + l.range * ((s.xPos + s.width) - l.xPos), s.yPos + s.height + l.range * (s.yPos + s.height- l.yPos));
+		shadow.addPoint(s.xPos , s.yPos + s.height);
+//		g.setColor(Color.red);
+		g.fillPolygon(shadow);
+		
+		shadow = new Polygon();
+		shadow.addPoint(s.xPos + s.width,  s.yPos + s.height);
+		shadow.addPoint(s.xPos + s.width + (l.range * (s.xPos + s.width - l.xPos)), s.yPos + s.height + l.range * (s.yPos + s.height - l.yPos));
+		shadow.addPoint(s.xPos + l.range * ((s.xPos) - l.xPos), s.yPos + s.height + l.range * (s.yPos + s.height- l.yPos));
+
+		shadow.addPoint(s.xPos , s.yPos + s.height);
+//		g.setColor(Color.green);
+		g.fillPolygon(shadow);
+		
+		
+		shadow = new Polygon();
+		shadow.addPoint(s.xPos + s.width, s.yPos + s.height);
+		shadow.addPoint(s.xPos + s.width + l.range * (s.xPos + s.width - l.xPos), s.yPos + s.height + l.range * (s.yPos + s.height- l.yPos));
+		shadow.addPoint(s.xPos + s.width + l.range * (s.xPos + s.width - l.xPos), s.yPos +  l.range * (s.yPos - l.yPos));
+
+		shadow.addPoint(s.xPos + s.width, s.yPos);
+//		g.setColor(Color.blue);
+		g.fillPolygon(shadow);
+		
+	}
 	public void setCamera(int x, int y, int sx, int sy) {
 		camX = x;
 		camY = y;
@@ -92,6 +155,41 @@ public class Engine extends Canvas {
 
 	public BufferedImage getScene() {
 		return buffImg;
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("bugger");
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
